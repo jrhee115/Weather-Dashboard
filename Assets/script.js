f0a8ee4a8345b511e8b239f9262abb415d7a9997
J Rhee <jrhee@Justins-MacBook-Pro.local>
2020-11-23 23:26:44 -0500

    added the 5 day forecast, however its not working. Now my UV index isnt working

--- Assets/script.js
+++ Assets/script.js
@@ -30,7 +30,7 @@ function searchWeather(searchValue){
         var temp = $("<p>").addClass("card-text").text("Temp: " + data.main.temp + "°F");
         var badgeURL = "http://openweathermap.org/img/w/" + data.weather[0].badge + ".png";
         var badge = $("img").attr("src", badgeURL);
-        var cardBody = $("<div>").addClass("card-body").attr("id", "searchWeather");
+        var cardBody = $("<div>").addClass("card-body");
 
         cardBody.append(title, date, wind, humid, temp);
         card.append(cardBody);
@@ -38,7 +38,7 @@ function searchWeather(searchValue){
         $("#today").append(card);
 
         getUV(data.coord.lat, data.coord.lon);
-        fiveDay(searchValue);
+        forecast(searchValue);
     });
 }
     //function to get UV index (another different URL)
@@ -64,5 +64,33 @@ function searchWeather(searchValue){
             uv.append(inDex);
             $("#searchWeather").append(uv);
         });
+    }
+    //5 day forceast
+    function forecast(searchValue) {
+        $.ajax({
+            type: "GET",
+            url: `https://api.openweathermap.org/data/2.5/forecast?q=${searchValue}&appid=b15857b75000df26bc3646e1cbb33de4`,
+            dataType: "JSON",
+        }).then(function (data) {
+            console.log(data)
+            for (var i = 4; i< data.list.length; i+=8){
+                var currentDate = data.list[i].dt_txt;
+                currentDate = currentDate.split("-");
+                var month = currentDate[1];
+                var day = currentDate[2].slice(0, 2);
+                var year = currentDate[0];
+                var title = $("<h5>").addClass("card-title").text(`Date: ${month} / ${day} / ${year}`);
+                var card = $("<div>").addClass("card");
+                var wind = $("<p>").addClass("card-text").text("Wind Speed: " + data.wind.speed + "mph");
+                var humid = $("<p>").addClass("card-text").text("Humidity: " + data.main.humidity + "%");
+                var temp = $("<p>").addClass("card-text").text("Temp: " + data.main.temp + "°F");
+                var badgeURL = "http://openweathermap.org/img/w/" + data.weather[0].badge + ".png";
+                var badge = $("img").attr("src", badgeURL);
+                var cardBody = $("<div>").addClass("card-body");
+                cardBody.append(title, badge, temp, humid, wind);
+                card.aooend(cardBody);
+                $("#today").append(card);
+            }
+        })
     }
 })
